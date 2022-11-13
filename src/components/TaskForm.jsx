ca5ae6159acac16e58acc750ca41f8015a229eb6
Lucas <luemadiaz1@gmail.com>
2022-11-13 01:40:40 -0300

    first commi

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -21,12 +21,13 @@ function TaskForm() {
     return (
     <div className='max-w-md  mx-auto'>
         <form onSubmit={handleSubmit}               className="bg-slate-800 p-10 mb-4" >
+
             <input placeholder="Escribe tu tarea" 
         onChange={(e)=> setTitle(e.target.value)}
         value={title}
         autoFocus
         className='bg-slate-300 p-3 w-full mb-2' />
-            <h1 className='text-2xl font-bold text-white mb-3'>Crea tu tarea</h1>
+         <h1 className='text-2xl font-bold text-white mb-3'>Crea tu tarea</h1>           
             <textarea placeholder='Escribe la descripcion aqui' onChange={(e)=>setDescription(e.target.value)}
         value={description} className='bg-slate-300 p-3 w-full mb-2' ></textarea>
             <button className='bg-indigo-500 px-3 py-1 text-white'>
